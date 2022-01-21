2fb91b6f83ecb5220f308b96491cb865a4797e7e
Jose Manuel <joslobtro@alum.us.es>
2022-01-21 11:22:22 +0100

    controlcheck

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,15 +2,41 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
+@Entity
 public class Care {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	
+	@NotEmpty
+	@Size(min = 5, max = 30)
+	@Column(name="name", unique=true)
     String name;
+	
+	@NotEmpty
     String description;
+    
+    @NotEmpty
+    @ManyToMany(cascade = CascadeType.ALL)
     Set<PetType> compatiblePetTypes;
 }
--- src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvision.java
@@ -1,5 +1,14 @@
 package org.springframework.samples.petclinic.care;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.samples.petclinic.pet.Visit;
 
 import lombok.Getter;
@@ -7,8 +16,22 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name="care_provision")
 public class CareProvision {   
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	
+	@NotNull
+	@Min(0)
     double duration;
+    
+	@ManyToOne
     Visit visit;
-    Care care;   
+    
+    @NotNull
+    @ManyToOne(optional=false)
+    Care care;
 }
